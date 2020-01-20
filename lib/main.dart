9f6b2d0e19e5a0069a91288b8b5b462d33160bf3
sergey <seregakdg1@gmail.com>
2020-01-20 20:57:07 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: new Scaffold(
+            body: Center(
+                child: new HomePage()
+            )
+        )
+    );
+  }
+}
+
+class HomePage extends StatefulWidget  {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("hi");
+  }
+
+
+}
