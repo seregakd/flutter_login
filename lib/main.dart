552f81a558da11d19e7e0d9581f229d627e31f7f
sergey <seregakdg1@gmail.com>
2020-01-22 22:12:45 +0200

    correct login form

--- lib/main.dart
+++ lib/main.dart
@@ -27,19 +27,13 @@ class LoginPage extends StatefulWidget {
   State<StatefulWidget> createState() => new _LoginPageState();
 }
 
-// Used for controlling whether the user is loggin or creating an account
-enum FormType {
-  login,
-  register
-}
-
 class _LoginPageState extends State<LoginPage> {
 
   final TextEditingController _emailFilter = new TextEditingController();
   final TextEditingController _passwordFilter = new TextEditingController();
   String _email = "";
   String _password = "";
-  FormType _form = FormType.login;
+
   _LoginPageState() {
     _emailFilter.addListener(_emailListen);
     _passwordFilter.addListener(_passwordListen);
@@ -61,17 +55,6 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
-  // Swap in between our two forms, registering and logging in
-  void _formChange() async {
-    setState(() {
-      if (_form == FormType.register) {
-        _form = FormType.login;
-      } else {
-        _form = FormType.register;
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -81,7 +64,10 @@ class _LoginPageState extends State<LoginPage> {
         child: new Column(
           children: <Widget>[
             _buildTextFields(),
-            _buildButtons(),
+            new RaisedButton(
+              child: new Text('Login'),
+              onPressed: _loginPressed,
+            ),
           ],
         ),
       ),
@@ -121,46 +107,7 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  Widget _buildButtons() {
-    if (_form == FormType.login) {
-      return new Container(
-        child: new Column(
-          children: <Widget>[
-            new RaisedButton(
-              child: new Text('Login'),
-              onPressed: _loginPressed,
-            ),
-            new FlatButton(
-              child: new Text('Dont have an account? Tap here to register.'),
-              onPressed: _formChange,
-            ),
-            new FlatButton(
-              child: new Text('Forgot Password?'),
-              onPressed: _passwordReset,
-            )
-          ],
-        ),
-      );
-    } else {
-      return new Container(
-        child: new Column(
-          children: <Widget>[
-            new RaisedButton(
-              child: new Text('Create an Account'),
-              onPressed: _createAccountPressed,
-            ),
-            new FlatButton(
-              child: new Text('Have an account? Click here to login.'),
-              onPressed: _formChange,
-            )
-          ],
-        ),
-      );
-    }
-  }
-
   void _loginPressed () {
-    print('The user wants to login with $_email and $_password');
     if (!users.containsKey(_email)) {
       _viewShowDialog('Username not exists');
     } else {
@@ -189,16 +136,6 @@ class _LoginPageState extends State<LoginPage> {
         ],
       ),
     );
-  }
-
-
-  void _createAccountPressed () {
-    print('The user wants to create an accoutn with $_email and $_password');
-
-  }
-
-  void _passwordReset () {
-    print("The user wants a password reset request sent to $_email");
   }
 
 
